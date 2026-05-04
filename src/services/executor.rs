3583b2a08a049272cc1d43396f7b23e62fb174a6
DavidMANZI-093 <manzidavid093@gmail.com>
2026-05-04 08:21:23 +0200

    fix: handle functional unmatched 'RuleStates', remove redundand 'scenario_matches' check, and fix complex command parsing with raw shell injection

--- src/services/executor.rs
+++ src/services/executor.rs
@@ -25,9 +25,10 @@ pub fn process_event(event: &PowerEvent, current_state: &mut SystemPowerState, c
         let scenario_matches: bool = match rule.state {
             RuleState::Charging => current_state.status == BatteryStatus::Charging,
             RuleState::Discharging => current_state.status == BatteryStatus::Discharging,
+            RuleState::Full => current_state.status == BatteryStatus::Full,
+            RuleState::NotCharging => current_state.status == BatteryStatus::NotCharging,
             RuleState::AcOnline => current_state.ac_online,
             RuleState::AcOffline => !current_state.ac_online,
-            _ => false,
         };
 
         if !scenario_matches {
@@ -44,47 +45,45 @@ pub fn process_event(event: &PowerEvent, current_state: &mut SystemPowerState, c
             None => true,
         };
 
-        if scenario_matches && threshold_met {
-            execute_rule(&rule.command, current_state.capacity <= 5);
+        if threshold_met {
+            execute_rule(
+                &rule.command,
+                current_state.capacity <= 5 && current_state.status == BatteryStatus::Discharging,
+            );
         }
     }
 }
 
 fn execute_rule(command_string: &str, is_critical: bool) {
-    let mut parts = command_string.split_whitespace();
+    let command_string_owned = command_string.to_string();
 
-    if let Some(binary) = parts.next() {
-        let binary_owned = binary.to_string();
-        let args: Vec<String> = parts.map(|s| s.to_string()).collect();
+    match Command::new("sh").args(["-c", command_string]).spawn() {
+        Ok(mut child) => {
+            info!("Executed rule: {}", command_string);
 
-        match Command::new(&binary_owned).args(&args).spawn() {
-            Ok(mut child) => {
-                info!("Executed rule: {}", command_string);
+            std::thread::spawn(move || {
+                if let Ok(status) = child.wait()
+                    && !status.success()
+                {
+                    warn!(
+                        "Command '{}' exited with an error code.",
+                        command_string_owned
+                    );
 
-                std::thread::spawn(move || {
-                    if let Ok(status) = child.wait()
-                        && !status.success()
-                    {
-                        warn!(
-                            "Command '{}' exited with an error code.",
-                            binary_owned.clone()
-                        );
-
-                        if is_critical {
-                            emergency_shutdown();
-                        }
+                    if is_critical {
+                        emergency_shutdown();
                     }
-                });
-            }
-            Err(e) => {
-                error!(
-                    "Critical: Failed to spawn command '{}'. Error: {}",
-                    binary, e
-                );
-
-                if is_critical {
-                    emergency_shutdown();
                 }
+            });
+        }
+        Err(e) => {
+            error!(
+                "Critical: Failed to spawn command '{}'. Error: {}",
+                command_string, e
+            );
+
+            if is_critical {
+                emergency_shutdown();
             }
         }
     }
